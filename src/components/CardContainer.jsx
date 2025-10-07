12a728c2dc8e1a7516bd99b88ddd91417aecaeeb
Pratush <pratushbarua26@gmail.com>
2025-10-07 18:59:08 +0600

    added bookmarked LIst

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,4 +1,4 @@
-import { use, useContext, useState } from "react";
+import { use, useContext, useState, useEffect } from "react";
 import CountryCard from "./Card";
 import { SearchContext } from "../context/context";
 
@@ -6,25 +6,41 @@ export default function CountryCardContainer({ data, refreshLocalStorage }) {
   const datam = use(data);
   const [modalData, setModalData] = useState(null);
 
+  // search fucntionality
   const { searchWord, selectedOption } = useContext(SearchContext)
-  const dataSet = searchWord ? datam.filter(e => 
-    selectedOption === 'name' ?
-    e.name.common.trim().toLowerCase().includes(searchWord.trim().toLowerCase())
-    :
-    selectedOption === 'languages' ?
-    Object.values(e.languages.languages).some(lang => lang.toLowerCase().includes(searchWord.trim().toLowerCase()))
-    :
-    selectedOption === 'region' ?
-    e.region.region.trim().toLowerCase().includes(searchWord.trim().toLowerCase())
-    :
-    selectedOption === 'capital' ?
-    e.capital.capital.find(f => f.toLowerCase().includes(searchWord.toLowerCase()))
-    :
-    selectedOption === 'continents' ?
-    e.continents.continents.find(f => f.toLowerCase().includes(searchWord.toLowerCase()))
-    :
-    e.ccn3.ccn3.includes(searchWord)
-) : datam
+  const [dataSet, setDataSet] = useState(datam)
+  const [list, setList] = useState('all')
+  useEffect(() => {
+    if (searchWord) {
+      setDataSet(datam.filter(e =>
+        selectedOption === 'name' ?
+          e.name.common.trim().toLowerCase().includes(searchWord.trim().toLowerCase())
+          :
+          selectedOption === 'languages' ?
+            Object.values(e.languages.languages).some(lang => lang.toLowerCase().includes(searchWord.trim().toLowerCase()))
+            :
+            selectedOption === 'region' ?
+              e.region.region.trim().toLowerCase().includes(searchWord.trim().toLowerCase())
+              :
+              selectedOption === 'capital' ?
+                e.capital.capital.find(f => f.toLowerCase().includes(searchWord.toLowerCase()))
+                :
+                selectedOption === 'continents' ?
+                  e.continents.continents.find(f => f.toLowerCase().includes(searchWord.toLowerCase()))
+                  :
+                  e.ccn3.ccn3.includes(searchWord))
+      )
+    }
+    else if(list === 'bookmarked'){
+      const localdata = JSON.parse(localStorage.getItem("bookmarkedCountries"))
+      setDataSet(datam.filter(e => localdata.includes(e.ccn3.ccn3)))
+    } 
+    else if(list === 'visited'){
+      const localdata = JSON.parse(localStorage.getItem("visitedCountries"))
+      setDataSet(datam.filter(e => localdata.includes(e.ccn3.ccn3)))
+    } 
+    else setDataSet(datam)
+  }, [searchWord, selectedOption, datam, list])
 
   return (
     <>
@@ -61,6 +77,10 @@ export default function CountryCardContainer({ data, refreshLocalStorage }) {
         </div>
       </dialog>
       {/* modal */}
+      <div className="flex justify-end gap-3 w-11/12 mx-auto">
+        <button onClick={() => setList(list === 'visited' ? 'all' : 'visited')} className={`px-4 py-2 rounded-sm cursor-pointer ${list === 'visited' ? 'bg-gray-700 border' : 'bg-gray-900'}`}>Visited Countries</button>
+        <button onClick={() => setList(list === 'bookmarked' ? 'all' : 'bookmarked')} className={`px-4 py-2 rounded-sm cursor-pointer ${list === 'bookmarked' ? 'bg-gray-700 border' : 'bg-gray-900'}`}>Bookmarked Countries</button>
+      </div>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 place-content-center p-4">
         {dataSet.map((e, i) => (
           <CountryCard
